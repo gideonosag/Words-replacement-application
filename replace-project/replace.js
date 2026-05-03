8bd4a90328da7002ef226d888323f7689caf55c9
Gideon Efosa <gideonefosa10@gmail.com>
2026-05-03 19:13:47 +0100

    Add files via upload

---
+++ replace-project/replace.js
@@ -0,0 +1,18 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let btn = document.getElementById("searchBtn");
+  btn.addEventListener("click", doFindAndReplace);
+});
+
+function doFindAndReplace(ev) {
+  ev.preventDefault();
+
+  let find = document.getElementById("find").value;
+  let replace = document.getElementById("replace").value;
+  let p = document.querySelector(".txt");
+
+  while (p.textContent.indexOf(find) != -1) {
+    p.textContent = p.textContent.replace(find, replace);
+  }
+}
